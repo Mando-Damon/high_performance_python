3b2b862cc1d8404e83023a476cf99642825c5125
Tang, Damon <damon.tang@intel.com>
2025-11-13 11:14:14 +0800

    nonblocking_acquire.py: add try finally

--- nonblocking_acquire.py
+++ nonblocking_acquire.py
@@ -12,14 +12,16 @@ def shopper():
     name = threading.current_thread().name
     items_to_add = 0
     while items_on_notepad <= 20:
-        if items_to_add and pencil.acquire(blocking=False): # add item(s) to shared items_on_notepad
-            items_on_notepad += items_to_add
-            print(name, 'added', items_to_add, 'item(s) to notepad.')
-            items_to_add = 0
-            time.sleep(0.3) # time spent writing
-            pencil.release()
-        else: # look for other things to buy
-            time.sleep(0.1) # time spent searching
+        if items_to_add and pencil.acquire(blocking=False):  # add item(s) to shared items_on_notepad
+            try:
+                items_on_notepad += items_to_add
+                print(name, 'added', items_to_add, 'item(s) to notepad.')
+                items_to_add = 0
+                time.sleep(0.3)  # time spent writing
+            finally:
+                pencil.release()
+        else:  # look for other things to buy
+            time.sleep(0.1)  # time spent searching
             items_to_add += 1
             print(name, 'found something else to buy.')
 
